704b43dbea6dd00cf8fac9055ea454eec79a0608
jenkins-inspire <dpetruk@cern.ch>
2018-02-06 17:42:37 +0100

    Remove template file afterwards

--- init.sh
+++ init.sh
@@ -5,3 +5,5 @@ minion_nodes=$(kubectl get nodes | tail -n +2 | grep -v 'master' | awk '{ print 
 for node in "${minion_nodes[@]}"; do
     sed -e "s/$$NAME$$/$node/g" service.yaml.tpl > "$node-service.yaml"
 done
+
+rm service.yaml.tpl
