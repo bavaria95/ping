83768444f7a5f3acdd77abf6f0498b632c509b2a
jenkins-inspire <dpetruk@cern.ch>
2018-02-07 13:36:48 +0100

    Using proper bash arrays

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-minion_nodes=$(kubectl get nodes | tail -n +2 | grep -v 'master' | awk '{ print $1 }')
+minion_nodes=($(kubectl get nodes | tail -n +2 | grep -v 'master' | awk '{ print $1 }'))
 
 for node in ${minion_nodes[@]}; do
     sed -e "s/@@NAME@@/$node/g" service.yaml.tpl > "$node-service.yaml"
